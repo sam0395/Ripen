6d5240c3e0ca7a962d5337d3000ccf66406e1f20
sam0395 <sam0395@yahoo.com>
2015-06-06 10:41:47 -0400

    added php

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 
+
+
 <html>
 	<head>
 		<title>Ripen</title>
 		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
 	</head>
+
+	<body>
+		<?php 
+			include_once('nav.php');
+		?>
+
+		<div class = "container">
+			<div class = "col-md-12"><center>Hi</center></div>
+		</div>
+		
+
+	</body>
 </html>
