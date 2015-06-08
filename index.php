9d7726d2458dfa88dc36e1e8d4d6dd04469368ae
sam0395 <sam0395@yahoo.com>
2015-06-08 12:34:59 -0400

    added function to update fixed background every 5 seconds

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="dist/css/style.css">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		
 		<script type="text/javascript" src="dist/js/bootstrap.js"></script>
 		<script src="dist/js/smooth-scroll.min.js"></script>
 	</head>
@@ -18,14 +20,28 @@
 			include_once('nav.php');
 		?>
 
-		
+			
+
 			<div id="home" class="col-md-12 header-image" id="main">
 				<div>
 					<h1 class="h1">Text Example</h1>
 				</div>
 				<div id="caret"><a data-scroll href="#about"><i class="icon-angle-down"></i></a></div>
 			</div>
+
+			<script type="text/javascript">
+			setInterval(function changeBackground(){
+
+					 if($('#home').prop("style").backgroundImage === ''){
+					 	$('#home').css("background-image", "url('../Ripen/images/header.jpg')");
+					 }else
+					 $('#home').css("background-image", "");
+					 
+			}, 5000);
+
 			
+			</script>
+
 			<?php 
 			include_once('remaining.php');
 			?>
