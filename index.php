4b3ee5679e8dc57eac53d3a3e2a3e70baeaf2133
sam0395 <sam0395@yahoo.com>
2015-06-08 18:48:22 -0400

    added background transitions

--- index.php
+++ index.php
@@ -30,15 +30,33 @@
 			</div>
 
 			<script type="text/javascript">
+			var transition = true;
+			var i = 1;
+			var images = [];
+
+			images[0] = "url('../Ripen/images/header.jpg')"
+			images[1] = "url('../Ripen/images/header2.jpg')"
+			images[2] = "url('../Ripen/images/header3.jpg')"
+			images[3] = "url('../Ripen/images/header4.jpg')"
+
+			console.log(images.length);
+			setTimeout(function firstFade() {$('#home').animate({'opacity': 0}, 13000);}, 3000);
+
 			setInterval(function changeBackground(){
+						if(i>images.length - 2){
+							i = 0;
+						}
+						else
+							i++;
 
-					 if($('#home').prop("style").backgroundImage === ''){
-					 	$('#home').css("background-image", "url('../Ripen/images/header.jpg')");
-					 }else
-					 $('#home').css("background-image", "");
-					 
-			}, 5000);
+						console.log(i);
 
+					 	$('#home').css({opacity : 0});
+					 	$('#home').animate({'opacity': 1}, 3000);
+					 	$('#home').css("background-image", images[i]);
+						$('#home').animate({'opacity': 0}, 13000);
+			}, 13000);
+		
 			
 			</script>
 
