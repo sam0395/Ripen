213cff043273b312e0cdab9793610911eb6721aa
brandon bronisz <bbroni1@live.spcollege.edu>
2015-07-31 11:28:26 -0400

    added working file

---
+++ brandon/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+
+
+<html>
+	<head>
+		<title>Ripen</title>
+		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="dist/css/style.css">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		
+		<script type="text/javascript" src="dist/js/bootstrap.js"></script>
+		<script src="dist/js/smooth-scroll.min.js"></script>
+	</head>
+
+	<body>
+		<?php 
+			include_once('nav.php');
+		?>
+
+			
+
+			<div id="home" class="col-md-12 header-image hidden-xs hidden-sm" id="main">
+				<div id="caret"><a data-scroll href="#services"><i class="icon-angle-down"></i></a></div>
+			</div>
+
+		
+
+			<?php 
+			include_once('remaining.php');
+			?>
+
+	</body>
+</html>
---
+++ brandon/remaining.php
@@ -0,0 +1,278 @@
+<div class="col-md-12">
+
+
+
+
+
+
+
+    <!-- Services -->
+    <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
+    <div id="services" class="services bg-primary">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-10 col-lg-offset-1">
+                 <div class="row">
+                    <div class="col-lg-12">
+                        <center><h1>Services
+
+                        </h1></center>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="service-item">
+                            <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-cloud fa-stack-1x text-primary"></i>
+                            </span>
+                            <h4>
+                                <strong>Web Design</strong>
+                            </h4>
+                            <p>Inherit an eloquent design that will amaze your customers every visit</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="service-item">
+                            <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-compass fa-stack-1x text-primary"></i>
+                            </span>
+                            <h4>
+                                <strong>E-Commerce</strong>
+                            </h4>
+                            <p>Expand your horizons by selling your products worldwide</p>
+
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="service-item">
+                            <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-flask fa-stack-1x text-primary"></i>
+                            </span>
+                            <h4>
+                                <strong>Social Media</strong>
+                            </h4>
+                            <p>Grow your business by reaching millions of people via social media</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="service-item">
+                            <span class="fa-stack fa-4x">
+                                <i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-shield fa-stack-1x text-primary"></i>
+                            </span>
+                            <h4>
+                                <strong>SEO</strong>
+                            </h4>
+                            <p>Exceed the competition by being known by the world's largest search engines</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.col-lg-10 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</div>
+
+
+
+<div id="portfolio">
+
+   <!-- Page Heading -->
+
+   <!-- /.row -->
+
+   <div class="container">
+       <div class="row">
+        <div class="col-lg-12">
+            <center><h1>Portfolio
+
+            </h1></center>
+        </div>
+    </div>
+
+
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-4 portfolio-item">
+            <a href="http://173.170.97.243/chem-stat">
+                <img class="img-responsive" src="images/chemstat1.png" alt="">
+            </a>
+            <h3>
+                <a href="http://173.170.97.243/chem-stat">Chem-Stat Inc.</a>
+            </h3>
+            <p>Chem-Stat Inc. exemplifies Ripen's basic website layout. No login processing, payment processing, or user feedback. Just a creative way for a company to pass on information.</p>
+        </div>
+        <div class="col-md-4 portfolio-item">
+            <a href="http://www.flightleveljobs.com">
+                <img class="img-responsive" src="images/flightleveljobs.png" alt="">
+            </a>
+            <h3>
+                <a href="http://www.flightleveljobs.com">Flight Level Jobs</a>
+            </h3>
+            <p>Flight Level Jobs represents the more advanced style that Ripen offers. It comes equipped with a login system for users to interact with the website on a personal basis. Flight Level Jobs also 
+                has a payment system built in. Everything a company needs to get known and make money!</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="http://www.justforyoufloraldesign.com">
+                    <img class="img-responsive" src="images/justforyoufloraldesign.png" alt="">
+                </a>
+                <h3>
+                    <a href="http://www.justforyoufloraldesign.com">Just For You Floral Design</a>
+                </h3>
+                <p>Just For You Floral Design is a unique twist on a wordpress template. The template was set up to allow a sole proprietor market her floral products without collecting payments online, but allowing reviews and simple 
+                    product management. This was a simple and quick solution that the proprietor was looking for at the time.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <!-- Projects Row -->
+            <div class="row">
+                <div class="col-md-4 portfolio-item">
+                    <a href="http://maritimecareersearch.com/demo/">
+                        <img class="img-responsive" src="images/maritimecareersearch.png" alt="">
+                    </a>
+                    <h3>
+                        <a href="http://maritimecareersearch.com/demo/">Maritime Career Search (Demo)</a>
+                    </h3>
+                    <p>Maritime Career Search is another example of Ripen's advance website. While it is still under developemnt, there is a demo available in which users can see how the maritime job search will work. 
+                        There is also a functioning admin page that works without a user name or password for testing purposes. </p>
+                    </div>
+
+                </div>
+                <!-- /.row -->
+
+
+
+
+
+
+
+            </div>
+
+
+        </div>
+
+
+
+        <div id="process" class="bg-primary">
+            <div id="container">
+               <div class="row">
+
+                <div class="col-lg-12">
+                    <center><h1>Our Process
+
+                    </h1></center>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4><i class="fa fa-fw fa-check"></i>Engage</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Ripen's development process begins way before a computer is touched. Ripen disects the customers needs, business strategy, and future goals. During the engage process, Ripen will work with the client to come to terms on how we will interact with them.</p>
+                            <a href="#" class="btn btn-default">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4><i class="fa fa-fw fa-gift"></i>Create</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Once terms have been reached, the client will be paired with an individual designer and programmer. A rough draft of the site will be created and semt to the client. This will be repeated until the client approves of the layout.</p>
+                            <a href="#" class="btn btn-default">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4><i class="fa fa-fw fa-compass"></i>Deliver</h4>
+                        </div>
+                        <div class="panel-body">
+                            <p>Once a layout is established, the designer and programmer will bring the layout to life. The website will go through an extensive testing phase and will be live only if the client has approved after testing. Once live, Ripen will support the website for 6 months (no charge).</p>
+                            <a href="#" class="btn btn-default">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div id="mid-image" class="hidden-sm hidden-xs">
+
+    </div>
+
+
+    <div id="contact">
+
+
+     <!-- Contact Section -->
+
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <center><h1>Contact Us
+
+                </h1></center>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <form name="sentMessage" id="contactForm" method ="post" action="email.php">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your Name *" name="name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Your Email *" name="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control" placeholder="Your Phone *" name="phone" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <textarea class="form-control" placeholder="Your Message *" name="message" required data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-lg-12 text-center">
+                            <div id="success"></div>
+                            <button type="submit" class="btn btn-xl">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+
+
+<div id="footer" class="bg-primary">
+  <p>footer example</p>
+</div>
+
+</div>
